d9d05a6e587c647a63398a5fca725abf87a1f052
JeremyWu0207 <106864061+JeremyWu0207@users.noreply.github.com>
2026-04-09 10:21:48 +0800

    Add matrix class definition with operations

---
+++ 0409/hw1/matrix.h
@@ -0,0 +1,19 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+class matrix {
+public:
+    int N;
+    long long mat[3][3];
+
+    matrix(int n = 2);
+    void print();
+    
+    // 依題目要求定義四個運算 method
+    void add(matrix* A, matrix* B);
+    void sub(matrix* A, matrix* B);
+    void multiple(matrix* A, matrix* B);
+    void divide(matrix* A, matrix* B);
+};
+
+#endif
